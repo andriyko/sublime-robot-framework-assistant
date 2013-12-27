58dca6e4871c444bcb1a30545da90061bd77f084
andriyko <ahrytskiv@gmail.com>
2013-12-27 13:28:55 +0200

    Fixed #3

--- rfassistant/load_keywords.py
+++ rfassistant/load_keywords.py
@@ -81,20 +81,19 @@ def get_keyword_at_pos(line, col):
     if length == 0:
         return None
     # between spaces
-    if all([col >= length or line[col] == ' ' or line[col] == "\t",
-            col == 0 or line[col-1] == ' ' or line[col-1] == "\t"]):
+    if ((col >= length or line[col] == ' ' or line[col] == "\t") and
+        (col == 0 or line[col-1] == ' ' or line[col-1] == "\t")):
         return None
     # first look back until we find 2 spaces in a row, or reach the beginning
     i = col - 1
     while i >= 0:
-        if line[i] == "\t" or all([line[i - 1] == ' ' or line[i - 1] == '|',
-                                   line[i] == ' ']):
+        if line[i] == "\t" or ((line[i - 1] == ' ' or line[i - 1] == '|') and line[i] == ' '):
             break
         i -= 1
     begin = i + 1
     # now look forward or until the end
     i = col  # previous included line[col]
-    while i < length - 1:
+    while i < length:
         if line[i] == "\t" or (line[i] == " " and len(line) > i and (line[i + 1] == " " or line[i + 1] == '|')):
             break
         i += 1
