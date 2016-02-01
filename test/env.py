dc964b95137c1460be6aabb18c4d40899b0e88b2
Tatu Aalto <aalto.tatu@gmail.com>
2016-02-01 20:55:25 +0200

    Moved files around

--- test/env.py
+++ test/env.py
@@ -7,7 +7,7 @@ ACCEPTANCE_TEST_DIR = path.join(ROOT_DIR, "acceptance")
 RESOURCES_DIR = path.join(ROOT_DIR, "resource")
 TEST_DATA_DIR = path.join(RESOURCES_DIR, "test_data")
 RESULTS_DIR = path.join(ROOT_DIR, "results")
-SRC_DIR = path.normpath(path.join(ROOT_DIR, "..", "src", 'dataparser'))
+SRC_DIR = path.normpath(path.join(ROOT_DIR, "..", 'dataparser'))
 
 sys.path.insert(0, SRC_DIR)
 sys.path.append(UNIT_TEST_DIR)
