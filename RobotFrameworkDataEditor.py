0e4d7774cf709f4cb21c4f87dab07185a44dbdb2
Tatu Aalto <aalto.tatu@gmail.com>
2016-02-03 21:53:39 +0200

    Initial version of the plugin settings

--- RobotFrameworkDataEditor.py
+++ RobotFrameworkDataEditor.py
@@ -3,15 +3,10 @@ Robot Framework from sublime is a autocompletion plugin for Sublime Text 3
 """
 import sys
 import os
-import logging
 from .commands import *
 
 if sys.version_info < (3, 3):
     raise RuntimeError('Plugin only works with Sublime Text 3')
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.setLevel(logging.DEBUG)
 
 
 def plugin_loaded():
