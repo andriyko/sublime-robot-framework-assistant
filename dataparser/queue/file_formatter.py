b356aa1815aadfdb58bbea927aace3c7cceb3ae2
Tatu Aalto <aalto.tatu@gmail.com>
2016-02-16 21:21:01 +0200

    Fixed encoding problem when used from Sublime 3

--- dataparser/queue/file_formatter.py
+++ dataparser/queue/file_formatter.py
@@ -3,6 +3,7 @@ from os import path
 
 
 def rf_table_name(f_path):
+    f_path = f_path.encode('utf-8')
     return '{realname}-{md5}.json'.format(
                 realname=path.basename(f_path),
                 md5=md5(f_path).hexdigest()
