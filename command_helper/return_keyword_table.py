363434cd49feac3bd72accbc264b38c3359438ae
Tatu Aalto <aalto.tatu@gmail.com>
2016-03-03 23:58:42 +0200

    Code to normalize line and return object and keyword from line.

--- command_helper/return_keyword_table.py
+++ command_helper/return_keyword_table.py
@@ -1,5 +1,9 @@
 import re
 from json import load as json_load
+try:
+    from current_view import KW_COMPLETION
+except:
+    from ..current_view import KW_COMPLETION
 
 
 class ReturnKeywordAndObject(object):
@@ -13,6 +17,21 @@ class ReturnKeywordAndObject(object):
         self.index_data = None
         self.current_view = current_view
         self.rf_cell = rf_cell
+
+    def normalize(self, line, column):
+        """Returns the keyword and object from the line.
+
+        ``line`` -- Full line text where cursor is.
+        ``column`` -- Location of the cursor
+        """
+        keyword = None
+        object_name = None
+        rf_cell = self.get_rf_cell(line, column)
+        if '.' not in rf_cell:
+            keyword = rf_cell
+        else:
+            object_name, keyword = self.separate_keyword_from_object(rf_cell)
+        return keyword, object_name
 
     def get_rf_cell(self, line, column):
         """Returns the cell from line where cursor is"""
@@ -30,6 +49,49 @@ class ReturnKeywordAndObject(object):
         elif to_column and not from_column:
             rf_cell = to_column.group()
         return rf_cell
+
+    def separate_keyword_from_object(self, rf_cell):
+        """Separates keyword from the object.
+
+        ``rf_cell`` -- cell where the cursor is.
+
+        ``rf_cell`` must be a valid valid keyword. Example
+        BuiltIn.Comment or Comment. ``rf_cell`` is separeted based
+        on the object names and keywords found from the
+        current_view.json file. If object and/or keyword can not be
+        found from the rf_cell, empty values are returned.
+        """
+        completions = self.data[KW_COMPLETION]
+        object_best_match = ''
+        keyword_best_match = ''
+        for kw_completion in completions:
+            object_name = kw_completion[2]
+            kw = kw_completion[0]
+            if rf_cell.startswith(object_name):
+                if len(object_best_match) <= len(object_name):
+                    object_canditate = object_name
+                    object_re = object_canditate.replace('.', '\\.')
+                    object_re = '(?:{0}\\.)(.+)'.format(object_re)
+                    match = re.search(object_re, rf_cell)
+                    if match:
+                        keyword_canditate = match.group(1)
+                    else:
+                        keyword_canditate = ''
+                    if self.kw_equals_kw_candite(kw, keyword_canditate):
+                        object_best_match = object_canditate
+                        keyword_best_match = keyword_canditate
+        return object_best_match, keyword_best_match
+
+    def kw_equals_kw_candite(self, kw, kw_candite):
+        """Returns True if kw == kw_canditate
+
+        Spaces, under score are removed and
+        strings are converted to lower before validation.
+        """
+        kw = kw.lower().replace(' ', '').replace('_', '')
+        kw_candite = kw_candite.lower().replace(' ', '').replace('_', '')
+        kw_candite = kw_candite.lstrip('.')
+        return kw == kw_candite
 
     def _get_data(self):
         f = open(self.current_view)
--- test/unit/test_get_kw_table_name.py
+++ test/unit/test_get_kw_table_name.py
@@ -1,5 +1,6 @@
 import unittest
 import env
+import json
 from os import path
 from return_keyword_table import ReturnKeywordAndObject
 
@@ -49,6 +50,128 @@ class TestCompletions(unittest.TestCase):
         rf_cell = self.rkao.get_rf_cell(line, column)
         self.assertEqual(rf_cell, kw_1_in_line)
 
+    def test_normalize_kw_only(self):
+        kw = 'Comment'
+        line = '{0}{1}'.format(self.rf_cell, kw)
+        column = len(line)
+        keyword, object_name = self.rkao.normalize(line, column)
+        self.assertEqual(keyword, kw)
+        self.assertEqual(object_name, None)
+
+    def test_normalize_kw_and_lib(self):
+        kw = 'Comment'
+        library = 'BuiltIn'
+        line = '{0}{1}.{2}'.format(self.rf_cell, library, kw)
+        column = len(line) - 1
+        keyword, object_name = self.rkao.normalize(line, column)
+        self.assertEqual(object_name, library)
+        self.assertEqual(keyword, kw)
+
+    def test_normalize_long_lib_and_space_in_kw(self):
+        kw = 'My Long Keyword'
+        library = 'com.company.library.DoLibrary'
+        line = '{0}{1}.{2}'.format(self.rf_cell, library, kw)
+        column = len(line) - 6
+        keyword, object_name = self.rkao.normalize(line, column)
+        self.assertEqual(object_name, library)
+        self.assertEqual(keyword, kw)
+
+    def test_separate_keyword_from_object_no_space(self):
+        kw = 'Comment'
+        library = 'BuiltIn'
+        cell = '{0}.{1}'.format(library, kw)
+        object_name, keyword = self.rkao.separate_keyword_from_object(cell)
+        self.assertEqual(object_name, library)
+        self.assertEqual(keyword, kw)
+
+    def test_separate_keyword_from_object_space(self):
+        kw = 'Import Library'
+        library = 'BuiltIn'
+        cell = '{0}.{1}'.format(library, kw)
+        object_name, keyword = self.rkao.separate_keyword_from_object(cell)
+        self.assertEqual(object_name, library)
+        self.assertEqual(keyword, kw)
+
+    def test_separate_keyword_from_object_lib_with_dots(self):
+        kw = 'My Long Keyword'
+        library = 'com.company.library.DoLibrary'
+        cell = '{0}.{1}'.format(library, kw)
+        object_name, keyword = self.rkao.separate_keyword_from_object(cell)
+        self.assertEqual(object_name, library)
+        self.assertEqual(keyword, kw)
+
+    def test_kw_equals_kw_candite(self):
+        kw1 = 'My Long Keyword'
+        kw2 = '.My Long Keyword'
+        self.assertTrue(self.rkao.kw_equals_kw_candite(kw1, kw2))
+        kw2 = 'Not Same'
+        self.assertFalse(self.rkao.kw_equals_kw_candite(kw1, kw2))
+        kw1 = 'My Long Keyword'
+        kw2 = '.my long keyword'
+        self.assertTrue(self.rkao.kw_equals_kw_candite(kw1, kw2))
+        kw1 = 'My Long Keyword'
+        kw2 = '.my_LONG_keyword'
+        self.assertTrue(self.rkao.kw_equals_kw_candite(kw1, kw2))
+        kw1 = 'My Long Keyword'
+        kw2 = '.myLONGkeyword'
+        self.assertTrue(self.rkao.kw_equals_kw_candite(kw1, kw2))
+
     def test_get_data(self):
         self.rkao._get_data()
         self.assertTrue('completion' in self.rkao.data)
+
+    @property
+    def add_dolibrary_kw1(self):
+        return [
+            "My Long Keyword", [
+                "arg1",
+                "arg2"
+                ],
+            "com.company.library.DoLibrary"
+        ]
+
+    @property
+    def add_dolibrary_kw2(self):
+        return [
+            "My Other Keyword", [
+                "arg1",
+                "arg2",
+                "arg3"
+                ],
+            "com.company.library.DoLibrary"
+        ]
+
+    @property
+    def add_otherlibrary_kw1(self):
+        return [
+            "My Long Keyword", [
+                "arg1",
+                "arg2"
+                ],
+            "com.company.library.OtherLibrary"
+        ]
+
+    @property
+    def add_spical_long_otherlibrary_kw1(self):
+        return [
+            "My Long Keyword", [
+                "arg1",
+                "arg2"
+                ],
+            "com.company.library.special.long.OtherLibrary"
+        ]
+
+    def do_setup(self):
+        f = open(self.current_view)
+        data = json.load(f)
+        f.close()
+        completion = data['completion']
+        completion.append(self.add_dolibrary_kw1)
+        completion.append(self.add_dolibrary_kw2)
+        completion.append(self.add_otherlibrary_kw1)
+        completion.append(self.add_spical_long_otherlibrary_kw1)
+        data['completion'] = completion
+        f = open(self.current_view, 'w')
+        json.dump(data, f, indent=4)
+        f.close()
+        self.do_setup = True
