05c1f576fd47da3566b4c46419413b531ffaa72c
Tatu Aalto <aalto.tatu@gmail.com>
2016-03-07 21:35:51 +0200

    Sublime command to show keyword documentation

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,9 +1,11 @@
 from .scan_index import ScanIndexCommand
 from .change_index import DetectViewChange
 from .query_completions import RobotCompletion
+from .show_documentation import ShowKeywordDocumentation
 
 __all__ = [
     'ScanIndexCommand',
     'DetectViewChange',
-    'RobotCompletion'
+    'RobotCompletion',
+    'ShowKeywordDocumentation'
 ]
---
+++ commands/show_documentation.py
@@ -0,0 +1,33 @@
+import sublime_plugin
+import sublime
+from ..setting.setting import get_setting
+from ..setting.setting import SettingObject
+from ..command_helper.current_view import CurrentView
+from ..command_helper.utils.get_text import get_line
+from ..command_helper.noralize_cell import ReturnKeywordAndObject
+from ..command_helper.get_metadata import get_rf_table_separator
+from ..command_helper.get_documentation import GetKeywordDocumentation
+
+
+class ShowKeywordDocumentation(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        w = sublime.active_window()
+        panel = w.create_output_panel('kw_documentation')
+        current_view = CurrentView()
+        workspace = get_setting(SettingObject.workspace)
+        open_tab = self.view.file_name()
+        index_db = get_setting(SettingObject.index_dir)
+        extension = get_setting(SettingObject.extension)
+        view_in_db = current_view.view_in_db(workspace, open_tab,
+                                             index_db, extension)
+        db_dir = get_setting(SettingObject.table_dir)
+        rf_cell = get_rf_table_separator(self.view)
+        if view_in_db:
+            line, column = get_line(self.view)
+            get_kw = ReturnKeywordAndObject(open_tab, rf_cell)
+            keyword, object_name = get_kw.normalize(line, column)
+            get_doc = GetKeywordDocumentation(db_dir, index_db, open_tab)
+            doc = get_doc.return_documentation(object_name, keyword)
+            panel.run_command('append', {'characters': doc})
+            w.run_command('show_panel', {'panel': 'output.kw_documentation'})
