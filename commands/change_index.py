a2d1d1cd9655d552faca48f06e808a2a8a759972
Tatu Aalto <aalto.tatu@gmail.com>
2016-03-25 01:23:44 +0200

    Fixed stack trace when opening other than Robot data files from workspace

--- commands/change_index.py
+++ commands/change_index.py
@@ -6,5 +6,6 @@ from ..command_helper.update_current_view_json import update_current_view_index
 class DetectViewChange(sublime_plugin.EventListener):
 
     def on_activated(self, view):
-        message = update_current_view_index(self.view)
-        sublime.status_message(message)
+        message = update_current_view_index(view)
+        if message:
+            sublime.status_message(message)
