edbbd9a29d7101be643d41a41858ca7f2af8872c
Tatu Aalto <aalto.tatu@gmail.com>
2016-05-11 23:03:32 +0300

    Fixes to perform index after file is changed

--- commands/change_index.py
+++ commands/change_index.py
@@ -1,11 +1,16 @@
 import sublime_plugin
 import sublime
 from ..command_helper.update_current_view_json import update_current_view_index
+from ..setting.setting import get_setting
+from ..setting.setting import SettingObject
 
 
 class DetectViewChange(sublime_plugin.EventListener):
 
     def on_activated(self, view):
-        message = update_current_view_index(view)
-        if message:
-            sublime.status_message(message)
+        if get_setting(SettingObject.automatic_index_creation):
+                view.run_command('index_open_tab')
+        else:
+            message = update_current_view_index(view)
+            if message:
+                sublime.status_message(message)
