87f60e25c154a268ef27835ea3fde45b15934b21
Tatu Aalto <aalto.tatu@gmail.com>
2016-09-01 22:55:31 +0300

    Removed change_index.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,6 @@
 from .scan_and_index import ScanIndexCommand
 from .scan import ScanCommand
 from .scan_open_tab import ScanOpenTabCommand
-from .change_index import DetectViewChange
 from .query_completions import RobotCompletion
 from .show_documentation import ShowKeywordDocumentation
 from .jump_to_keyword import JumpToKeyword
@@ -14,7 +13,6 @@ __all__ = [
     'ScanIndexCommand',
     'ScanCommand',
     'ScanOpenTabCommand',
-    'DetectViewChange',
     'RobotCompletion',
     'ShowKeywordDocumentation',
     'JumpToKeyword',
