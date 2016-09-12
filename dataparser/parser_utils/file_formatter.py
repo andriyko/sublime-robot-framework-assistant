c0bcade3d89795f7b4b87cc4d217ad007c4fafd9
Tatu Aalto <aalto.tatu@gmail.com>
2016-09-12 23:18:32 +0300

    Fixes too long filena by shorten the filenames in database
    (#159)

--- dataparser/parser_utils/file_formatter.py
+++ dataparser/parser_utils/file_formatter.py
@@ -5,13 +5,13 @@ from os import path
 def rf_table_name(f_path):
     md5sum = md5(f_path.encode('utf-8')).hexdigest()
     return '{realname}-{md5}.json'.format(
-        realname=path.basename(f_path),
+        realname=path.basename(f_path)[-100:],
         md5=md5sum
     )
 
 
 def lib_table_name(library):
     return '{realname}-{md5}.json'.format(
-        realname=library,
+        realname=library[-100:],
         md5=md5(library).hexdigest()
     )
---
+++ test/resource/test_data/suite_tree/really_long_folder_name_to_test_windows_problem_with_too_and_really_long_names_I_hope_windows_could_really_fix_their_apis_and_python_to_use_the_new_api_when_it.robot
@@ -0,0 +1,7 @@
+*** Settings ***
+Library           OperatingSystem
+
+*** Keywords ***
+Keyword From The Really Long File Name
+    [Arguments]    ${kwa1}
+    Log    ${kwa1}
